a0b64ce26b22cb4845715da2a178841e31f4e140
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-10-25 23:59:56 +0200

    updated

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const DATABASE_URL =
-  "mongodb+srv://admin:SPnM3Av7jJ6hBl7N@book-store-mern.bnxc6yw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store-mern";
+  "mongodb+srv://admin:SPnM3Av7jJ6hBl7N@book-store-mern.bnxc6yw.mongodb.net/book-collections?retryWrites=true&w=majority&appName=book-store-mern";
